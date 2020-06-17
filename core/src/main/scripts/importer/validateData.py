fddffdf52c9fc69f4526f6f9bc09e46da17faae7
Joel Nothman <joel.nothman@gmail.com>
2020-06-17 17:15:01 +1000

    Undo erroneous insertion of whitespace

--- core/src/main/scripts/importer/validateData.py
+++ core/src/main/scripts/importer/validateData.py
@@ -20,11 +20,7 @@
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <
-
-
-
-//www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 """Data validation script - validate files before import into portal.
